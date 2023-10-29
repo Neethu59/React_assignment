0731867103e60a6cda7cb64ebd76856c18e01c25
Neethu <neethurameesh2@gmail.com>
2023-10-29 16:44:28 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import User from './Pages/User/User';
+// import Work from './Pages/WorkDetails/Work';
+import { useState } from 'react';
+import User from './Page/User/User';
+import Work from './Page/WorkDetails/Work';
 function App() {
+  const [value,setValue]=useState()
+  const handlename=(data)=>{
+  console.log(data);
+  setValue(data)
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<User handlename={handlename}/>}></Route>
+      <Route path="/work" element={<Work value={value}/>}></Route>
+      </Routes>
+      </BrowserRouter>
   );
 }
 
